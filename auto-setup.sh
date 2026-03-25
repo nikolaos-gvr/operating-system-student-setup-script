2dd8b332e85e1a9158b4a69a5015d87f33ef63b7
Niko Gvr <126887434+nikgvr@users.noreply.github.com>
2026-03-25 14:39:46 +0200

    Modify setup script to indicate unsupported distros
    Updated informational messages for unsupported distributions.

--- auto-setup.sh
+++ auto-setup.sh
@@ -23,10 +23,10 @@ echo "[INFO] -> Detected distribution: $DISTRO"
 
 case "$DISTRO" in
     arch)
-        echo "[INFO] -> auto-setup: enabling extra repos for Arch Linux"
-        echo "[INFO] -> auto-setup: prepearing for setup of Arch Linux"
-        echo "[INFO] -> auto-setup: installing "student" packages for Arch Linux"
-        echo "[INFO] -> auto-setup: installing gaming related packages for Arch Linux"
+        #echo "[INFO] -> auto-setup: enabling extra repos for Arch Linux"
+        #echo "[INFO] -> auto-setup: prepearing for setup of Arch Linux"
+        #echo "[INFO] -> auto-setup: installing "student" packages for Arch Linux"
+        #echo "[INFO] -> auto-setup: installing gaming related packages for Arch Linux"
         #sh ./Arch-Linux/arch-enable-extra-repos.sh
         #sh ./Arch-Linux/arch-preparation-setup.sh
         #sh ./Arch-Linux/arch-student-setup.sh
@@ -58,10 +58,11 @@ case "$DISTRO" in
         ;;
 
     opensuse-tumbleweed|opensuse)
-        echo "[INFO] -> auto-setup: enabling extra repos for OpenSUSE Tumbleweed"
-        echo "[INFO] -> auto-setup: prepearing for setup of OpenSUSE Tumbleweed"
-        echo "[INFO] -> auto-setup: installing "student" packages for OpenSUSE Tumbleweed"
-        echo "[INFO] -> auto-setup: installing gaming related packages for OpenSUSE Tumbleweed"
+        echo "[INFO] -> OpenSUSE Tumbleweed was detected but it is required to inform you that even if this distro is intended to be supported the scripts have not been made yet"
+        #echo "[INFO] -> auto-setup: enabling extra repos for OpenSUSE Tumbleweed"
+        #echo "[INFO] -> auto-setup: prepearing for setup of OpenSUSE Tumbleweed"
+        #echo "[INFO] -> auto-setup: installing "student" packages for OpenSUSE Tumbleweed"
+        #echo "[INFO] -> auto-setup: installing gaming related packages for OpenSUSE Tumbleweed"
         #sh ./OpenSUSE-Tumbleweed/SUSE-Tumble-enable-extra-repos.sh
         #sh ./OpenSUSE-Tumbleweed/SUSE-Tumble-preparation-setup.sh
         #sh ./OpenSUSE-Tumbleweed/SUSE-Tumble-student-setup.sh
@@ -69,11 +70,12 @@ case "$DISTRO" in
         ;;
 
     linuxmint|ubuntu) 
+        echo "[INFO] -> Ubuntu/Mint was detected but it is required to inform you that even if this distro is intended to be supported the scripts have not been made yet"
         #remove ubuntu when you finish testing, ubuntu is only an option for testing purposes
-        echo "[INFO] -> auto-setup: enabling extra repos for Linux Mint"
-        echo "[INFO] -> auto-setup: prepearing for setup of Linux Mint"
-        echo "[INFO] -> auto-setup: installing "student" packages for Linux Mint"
-        echo "[INFO] -> auto-setup: installing gaming related packages for Linux Mint"
+        #echo "[INFO] -> auto-setup: enabling extra repos for Linux Mint"
+        #echo "[INFO] -> auto-setup: prepearing for setup of Linux Mint"
+        #echo "[INFO] -> auto-setup: installing "student" packages for Linux Mint"
+        #echo "[INFO] -> auto-setup: installing gaming related packages for Linux Mint"
         #sh ./Linux-Mint/mint-enable-extra-repos.sh
         #sh ./Linux-Mint/mint-preparation-setup.sh
         #sh ./Linux-Mint/mint-student-setup.sh
@@ -81,10 +83,11 @@ case "$DISTRO" in
         ;;
 
     fedora)
-        echo "[INFO] -> auto-setup: enabling extra repos for Fedora Linux"
-        echo "[INFO] -> auto-setup: prepearing for setup of Fedora Linux"
-        echo "[INFO] -> auto-setup: installing "student" packages for Fedora Linux"
-        echo "[INFO] -> auto-setup: installing gaming related packages for Fedora Linux"
+        echo "[INFO] -> Fedora was detected but it is required to inform you that even if this distro is intended to be supported the scripts have not been made yet"
+        #echo "[INFO] -> auto-setup: enabling extra repos for Fedora Linux"
+        #echo "[INFO] -> auto-setup: prepearing for setup of Fedora Linux"
+        #echo "[INFO] -> auto-setup: installing "student" packages for Fedora Linux"
+        #echo "[INFO] -> auto-setup: installing gaming related packages for Fedora Linux"
         #sh ./Fedora-Linux/fedora-enable-extra-repos.sh
         #sh ./Fedora-Linux/fedora-preparation-setup.sh
         #sh ./Fedora-Linux/fedora-student-setup.sh
